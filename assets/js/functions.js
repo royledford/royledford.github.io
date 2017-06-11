00d0b51c21eedb5abc593cce7c47c4922efff221
roy ledford <bluesixty@gmail.com>
2017-06-11 13:27:30 -0400

    add favicon

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,6 +2,18 @@ $( document ).ready(function() {
 
   // Get started!
 
+  //-----------------------------------------
+  // Remove current navigation highlight in header when clicked
+  //-----------------------------------------
+  $(".navbutton").click(function() {
+    console.log('test');
+    var $this = $(this);
+      // $("#ccc").toggle().animate();
+      $this.toggleClass('active');
+      $this.siblings().removeClass("active");
+
+  });
+
 
 
 });
