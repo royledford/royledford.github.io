e14939ea61f54306efa28487e0e2a7a0cf6beba3
Roy Ledford <bluesixty@gmail.com>
2024-02-13 09:58:10 -0500

    Move to vite

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div style={{ marginBottom: '40px' }}>
+        <span className="title">Roy Ledford</span>
+      </div>
+
+      <div className="links">
+        <a href="mailto:roy@royledford.com">
+          <span style={{ marginRight: '40px' }}>roy@royledford.com</span>
+        </a>
+        <a href="https://www.linkedin.com/in/royledford/" className="linkedin">
+          <span>linkedin.com/in/royledford/</span>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default App
