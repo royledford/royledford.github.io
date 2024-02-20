12695cd895f5f7573250a58aea55911bfdd44127
Roy Ledford <bluesixty@gmail.com>
2024-02-20 11:26:29 -0500

    cleanup

--- src/pages/Projects/projectDocs/ReactSvgMeters.jsx
+++ src/pages/Projects/projectDocs/ReactSvgMeters.jsx
@@ -2,7 +2,5 @@ import Article from '../../../components/Article/Article'
 import doc from './react-svg-meters.md'
 
 export default function ReactSvgMeters() {
-  console.log('doc', doc) // TODO: remove this
-
   return <Article markdownContent={doc} />
 }
